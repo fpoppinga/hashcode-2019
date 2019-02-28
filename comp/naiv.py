c0c8956ce9be4db189f7e0d534091c8c374b370c
Georg Felbinger <georg.felbinger@freiheit.com>
2019-02-28 19:40:04 +0100

    naiv.py

--- comp/naiv.py
+++ comp/naiv.py
@@ -1,15 +1,30 @@
 from comp.input import Problem
 from comp.out import Solution, Slide, zipdir
+from comp.score import score_of_interest, score
 
 
 def solve(problem: Problem) -> Solution:
-    return Solution([Slide(problem.photos[0])])
+    solution = Solution([])
+    for id, photo in problem.photos.items():
+        if photo.is_vertical:
+            continue
+        slide = Slide(photo)
+        if len(solution.slides) == 0:
+            solution.slides.append(slide)
+            continue
+        score = score_of_interest(solution.slides[-1], slide)
+        if score > 0:
+            solution.slides.append(slide)
+
+    return solution
 
 
 if __name__ == "__main__":
 
-    for file in ["a_example.txt", "b_lovely_landscapes.txt", "c_memorable_moments.txt", "d_pet_pictures.txt", "e_shiny_selfies.txt"]:
+    for file in ["a_example.txt", "b_lovely_landscapes.txt", "c_memorable_moments.txt", "d_pet_pictures.txt",
+                 "e_shiny_selfies.txt"]:
         p = Problem("in/{}".format(file))
         s = solve(p)
+        print("{} → {}".format(file, score(p, s)))
         s.to_file("out/{}".format(file))
     zipdir(".", "../code.zip")
